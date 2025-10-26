f8f96d41b74713eebebae033bac8892279abe487
broskoy <mihnea.buzoiu@gmail.com>
2025-10-26 20:50:09 +0100

    more ui tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { createSignal } from 'solid-js'
+
 import solidLogo from './assets/frameworks/solid.svg'
 import viteLogo from './assets/frameworks/vite.svg'
+import plusImage from './assets/frameworks/plus.png'
+
 import solarLogo from './assets/logo/solar_transparent.png'; 
 import nameLogo from './assets/logo/name_transparent.png';
+
 import './App.css'
 
 
@@ -32,10 +36,13 @@ function App() {
         </button>
       </div>
 
-      <div>
+      <div class="frameworks">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
+
+        <img src={plusImage} class="separator" alt="plus" />
+
         <a href="https://solidjs.com" target="_blank">
           <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
